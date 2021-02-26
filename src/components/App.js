55da9c632a3435d73080c5750c92283ed21dfab5
Erick Avila <nmr.inc@icloud.com>
2021-02-26 02:15:32 -0600

    Class 6. Adding Functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 const App = () => {
+	const ponerFilas = () => [
+		<tr>
+			<td>Erick</td>
+			<td>nmr.inc@icloud.com</td>
+			<td>nrm_inc.com</td>
+		</tr>,
+		<tr>
+			<td>Platzi</td>
+			<td>platzi@platzi.com</td>
+			<td>platzi.com</td>
+		</tr>,
+	];
+
 	return (
 		<div className="margen">
 			<table className="tabla">
@@ -9,18 +22,7 @@ const App = () => {
 					<th>Mail</th>
 					<th>URL</th>
 				</thead>
-				<tbody>
-					<tr>
-						<td>Erick</td>
-						<td>nmr.inc@icloud.com</td>
-						<td>nrm_inc.com</td>
-					</tr>
-					<tr>
-						<td>Platzi</td>
-						<td>platzi@platzi.com</td>
-						<td>platzi.com</td>
-					</tr>
-				</tbody>
+				<tbody>{ponerFilas()}</tbody>
 			</table>
 		</div>
 	);
