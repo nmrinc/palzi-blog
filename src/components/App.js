ebb2c8cc14e9766eef7917c0fc13f609b2e54749
Erick Avila <nmr.inc@icloud.com>
2021-02-25 22:51:58 -0600

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className="margen">
+			<table className="tabla">
+				<thead>
+					<th>Name</th>
+					<th>Mail</th>
+					<th>URL</th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Erick</td>
+						<td>nmr.inc@icloud.com</td>
+						<td>nrm_inc.com</td>
+					</tr>
+					<tr>
+						<td>Platzi</td>
+						<td>platzi@platzi.com</td>
+						<td>platzi.com</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default App;
