75d01974f96fff0a549db386aa23c5c255a72542
Erick Avila <nmr.inc@icloud.com>
2021-02-26 03:22:25 -0600

    Class 10. React Router DOM

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,16 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Menu from './Menu';
+import Users from './Users';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			users: [],
-		};
-	}
+const Tasks = () => <div>Tasks</div>;
 
-	async componentDidMount() {
-		const response = await axios.get(
-			'https://jsonplaceholder.typicode.com/users'
-		);
-
-		this.setState({
-			users: response.data,
-		});
-	}
-
-	ponerFilas = () =>
-		this.state.users.map((item) => (
-			<tr key={item.id}>
-				<td>{item.name}</td>
-				<td>{item.email}</td>
-				<td>{item.website}</td>
-			</tr>
-		));
-
-	render() {
-		return (
-			<div className="margen">
-				<table className="tabla">
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Mail</th>
-							<th>URL</th>
-						</tr>
-					</thead>
-					<tbody>{this.ponerFilas()}</tbody>
-				</table>
-			</div>
-		);
-	}
-}
+const App = () => (
+	<BrowserRouter>
+		<Menu />
+		<Route exact path="/" component={Users} />
+		<Route exact path="/tasks" component={Tasks} />
+	</BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Users/index.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Users extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: [],
+		};
+	}
+
+	async componentDidMount() {
+		const response = await axios.get(
+			'https://jsonplaceholder.typicode.com/users'
+		);
+
+		this.setState({
+			users: response.data,
+		});
+	}
+
+	ponerFilas = () =>
+		this.state.users.map((item) => (
+			<tr key={item.id}>
+				<td>{item.name}</td>
+				<td>{item.email}</td>
+				<td>{item.website}</td>
+			</tr>
+		));
+
+	render() {
+		return (
+			<div className="margen">
+				<table className="tabla">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Mail</th>
+							<th>URL</th>
+						</tr>
+					</thead>
+					<tbody>{this.ponerFilas()}</tbody>
+				</table>
+			</div>
+		);
+	}
+}
+
+export default Users;
