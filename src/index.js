13c767af2bd3e3cdcfda448384891c0fc54af96c
Erick Avila <nmr.inc@icloud.com>
2021-02-26 20:57:12 -0600

    Class 13. Store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(
+	{}, //@o Reducers
+	{} //@o Initial state
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
