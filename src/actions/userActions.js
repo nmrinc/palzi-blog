8b8a61025f605cf27bcfc1d2d83362a8c16bc434
Erick Avila <nmr.inc@icloud.com>
2021-03-01 20:35:34 -0600

    Classes 22.Async | 23.LoaderComponent

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,21 +2,23 @@ import { GET_USERS } from '../types/usersTypes';
 import axios from 'axios';
 
 export const getUsers = () => async (dispatch) => {
+	dispatch({
+		type: `${GET_USERS}_PENDING`,
+	});
+
 	try {
 		const response = await axios.get(
 			'https://jsonplaceholder.typicode.com/users'
 		);
 
 		dispatch({
-			type: GET_USERS,
+			type: `${GET_USERS}_FULFILLED`,
 			payload: response.data,
 		});
 	} catch (error) {
-		console.log('====================================');
-		console.error(
-			`Error found:
-			${error.message}`
-		);
-		console.log('====================================');
+		dispatch({
+			type: `${GET_USERS}_REJECTED`,
+			payload: error.message,
+		});
 	}
 };
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,13 +1,21 @@
 import { GET_USERS } from '../types/usersTypes';
 
 const INITIAL_STATE = {
+	isLoading: false,
 	users: [],
+	errorMsg: null,
 };
 
 const usersReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
-		case GET_USERS:
-			return { ...state, users: action.payload };
+		case `${GET_USERS}_PENDING`:
+			return { ...state, isLoading: true };
+
+		case `${GET_USERS}_FULFILLED`:
+			return { ...state, isLoading: false, users: action.payload };
+
+		case `${GET_USERS}_REJECTED`:
+			return { ...state, isLoading: false, errorMsg: action.payload };
 
 		default:
 			return state;
