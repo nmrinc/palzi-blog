bcf3d0d3900cefa3f507a840358eb7aed861cffa
Erick Avila <nmr.inc@icloud.com>
2021-03-05 02:33:50 -0600

    Class 46. Maping Objects

--- src/components/General/Fatal.jsx
+++ src/components/General/Fatal.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSkullCrossbones } from '@fortawesome/free-solid-svg-icons';
 
 const Fatal = (props) => (
-	<>
-		<h1>This Shit Crashed!</h1>
-		<h2>Please try again later</h2>
-		<small>{props.errMsg}</small>
-	</>
+	<div className="container">
+		<div className="col">
+			<FontAwesomeIcon
+				icon={faSkullCrossbones}
+				size="5x"
+				style={{ color: 'rgb(180,0,0)' }}
+			/>
+		</div>
+		<div className="col">
+			<h1>This Shit Crashed!</h1>
+			<h2>Please try again later</h2>
+			<small>{props.errMsg}</small>
+		</div>
+	</div>
 );
 export default Fatal;
